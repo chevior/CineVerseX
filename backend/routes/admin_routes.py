0b42728df10a37ae5f8b97c9f61ec72405c226f8
Chethan N <nchethan066@gmail.com>
2026-06-25 14:15:46 +0530

    first deploy

--- backend/routes/admin_routes.py
+++ backend/routes/admin_routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 
+from extensions import db
 from models.user import User
 from models.movie import Movie
 from models.theater import Theater
 from models.booking import Booking
 from models.ticket import Ticket
 from models.show import Show
+from models.booking import Payment
 
 admin_bp = Blueprint("admin_bp", __name__)
 
@@ -25,8 +27,6 @@ def admin_dashboard():
 
     total_revenue = 0
 
-    tickets = Ticket.query.all()
-
     for ticket in tickets:
         total_revenue += ticket.total_amount
 
@@ -38,8 +38,13 @@ def admin_dashboard():
         total_bookings=total_bookings,
         total_revenue=total_revenue
     )
+
+
 @admin_bp.route("/admin/users")
 def manage_users():
+
+    if session.get("user_role") != "admin":
+        return "Access denied. Admin only."
 
     users = User.query.all()
 
@@ -47,3 +52,39 @@ def manage_users():
         "manage_users.html",
         users=users
     )
+
+
+@admin_bp.route("/admin/delete-user/<int:user_id>")
+def delete_user(user_id):
+
+    if session.get("user_role") != "admin":
+        return "Access denied"
+
+    user = User.query.get_or_404(user_id)
+
+    if user.role == "admin":
+        return "Cannot delete admin"
+
+    user_bookings = Booking.query.filter_by(user_id=user.id).all()
+
+    for booking in user_bookings:
+
+        payment = Payment.query.filter_by(
+            booking_id=booking.id
+        ).first()
+
+        if payment:
+            db.session.delete(payment)
+
+        db.session.delete(booking)
+
+    user_tickets = Ticket.query.filter_by(user_id=user.id).all()
+
+    for ticket in user_tickets:
+        db.session.delete(ticket)
+
+    db.session.delete(user)
+
+    db.session.commit()
+
+    return redirect(url_for("admin_bp.manage_users"))
